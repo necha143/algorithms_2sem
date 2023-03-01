22a74ca1258d5bdfb451360edee18a351db7d7fe
Alexander Nechaev <nechaalex143@gmail.com>
2023-03-01 19:09:26 +0300

    added lab1

--- lab1(8)/massage.cpp
+++ lab1(8)/massage.cpp
@@ -1,98 +1,71 @@
-/*
 #include <iostream>
-#include <vector>
+#include <cmath>
+
 using namespace std;
-string MashaCode, SashaCode;
-vector<pair<int, char>> MashaMas; //pair позволяет обрабатывать два объекта как один
-vector<pair<int, char>> SashaMas; // данная штука будет хранить в int-количество повторений буквы, а в char-данную букву
 
-
-class decoder{
-public:
-    void masha (int n){
-        for (int i=0; i<n; i++) {
-            if (MashaCode[i]>47 && MashaCode[i]<58){
-                MashaMas[MashaMas.size()-1].first = MashaMas[MashaMas.size()-1].first * 10 + (MashaCode[i]-'0');
-            } else {
-                MashaMas.push_back(make_pair(0, MashaCode[i])); // Функция шаблона, которую можно использовать для построения объектов типа pair
-            }
-        }
+int maxPow(int length) {
+    int myPow = 0;
+    while (pow(2, myPow) < length) {
+        ++myPow;
     }
-
-    void sasha (int m){
-        for (int i=0; i<m; i++) {
-            if (SashaCode[i]>47 && SashaCode[i]<58){
-                SashaMas[SashaMas.size()-1].first = SashaMas[SashaMas.size()-1].first * 10 + (SashaCode[i]-'0');
-            } else {
-                SashaMas.push_back(make_pair(0, SashaCode[i]));
-            }
-        }
-    }
-} Decoder;
-
-
-void Display (int c, int k){
-    c>k? cout << "No" : cout <<"Yes";
+    return myPow;
 }
 
-
-void check (int n, int m, int k){
-    Decoder.masha(n);
-    Decoder.sasha(m);
-    //cout << MashaMas.size() << " " << SashaMas.size();
-
-    */
-/*int sum_of_ch;
-    for (int i=0; i<MashaMas.size(); i++){
-        sum_of_ch+=MashaMas[i].first;
-    }
-    if (sum_of_ch<k){
-        cout << "Yes";
-        return;
-    }*//*
-
-
-    int i=0, j=0, c=0;
-    while (i+j < MashaMas.size() + SashaMas.size()){
-
-        if (MashaMas[i].first < SashaMas[j].first){ // сравнение повторений количества некой буквы
-            SashaMas[j].first -= MashaMas[i].first; // в удачном случае, уменьшим количество для будущей проверки
-            if (MashaMas[i].second != SashaMas[j].second){ // ищем отличия
-                c+=MashaMas[i].first;
-            }
-            i++;
-
-        } else if (MashaMas[i].first > SashaMas[j].first){
-            MashaMas[i].first -= SashaMas[j].first;
-            if (MashaMas[i].second != SashaMas[j].second){
-                c+=SashaMas[j].first;
-            }
-            j++;
-
-        } else {
-            if (MashaMas[i].second != SashaMas[j].second){
-                c+=SashaMas[j].first;
-            }
-            i++; j++;
+bool isDegreeOfTwo(int x) {
+    bool res = true;
+    while (x > 0) {
+        if (x == 1) {
+            return res;
+        } else if (x % 2 != 0) {
+            return false;
         }
-
+        x /= 2;
     }
-
-    Display(c,k);
-    return;
-};
-
-
+}
 
 int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(0); cout.tie(0);
-    int n, m, k;
-    cin >> n >> m >> k;
-    cin >> MashaCode;
-    cin >> SashaCode;
-    check(n,m,k);
-
-    return 0;
+    int t;
+    cin >> t;
+    for (int k = 0; k < t; k++) {
+        string str;
+        cin >> str;
+        int strPow = maxPow(str.length());
+        int bitArr[strPow];
+        for (int i = 0; i < strPow; i++) {
+            bitArr[i] = 0;
+        }
+        int index = 0;
+        int step = 1;
+        while (step < pow(2, strPow)) {
+            int stepControl = 0;
+            for (int i = step - 1; i < str.length(); i++) {
+                if (str[i] == '1') {
+                    bitArr[index]++;
+                }
+                stepControl++;
+                if (stepControl == step) {
+                    stepControl = 0;
+                    i += step;
+                }
+            }
+            step += step;
+            index++;
+        }
+        int mistake = 0;
+        for (int i = 0; i < strPow; i++) {
+            if (bitArr[i] % 2 == 1) {
+                mistake += pow(2, i);
+            }
+        }
+        if (mistake) {
+            str[mistake - 1] = str[mistake - 1] == '1' ? '0' : '1';
+        }
+        string myStr = "";
+        for (int i = 0; i < str.length(); i++) {
+            if (!isDegreeOfTwo(i + 1)) {
+                myStr.push_back(str[i]);
+            }
+        }
+        cout << myStr << endl;
+    }
 }
-*/
